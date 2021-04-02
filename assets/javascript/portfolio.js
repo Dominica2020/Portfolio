f40e90e670dd4796cad10f75c258cacbdd9b1149
Gecko Uno <dominica.patterson@gmail.com>
2021-04-02 10:52:33 -0700

    update to CSS

---
+++ assets/javascript/portfolio.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    document.getElementById("hidden-menu").style.display = "unset";
+   
+   document.getElementById("hamburger").style.display = "none";
+   
+   document.getElementById("close").style.display = "unset";
+   
+ }
+ 
+ function closeMenu() {
+   document.getElementById("close").style.display = "none";
+   
+   document.getElementById("hidden-menu").style.display = "none";
+   
+   document.getElementById("hamburger").style.display = "unset";
+   
+ }
